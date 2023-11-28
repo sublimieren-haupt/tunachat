083485f115baf326e31c69f7a2219b8ac3a714b8
sublimieren-haupt <151779539+sublimieren-haupt@users.noreply.github.com>
2023-11-29 00:45:06 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+const major = 0
+const minor = 2
+const patch = 1
+const security = 0
+
+var c_init = false
+
+
 var firebaseConfig = {
     apiKey: "AIzaSyCcgbrX_UKi_HYNxwgFQqb18cd6uimK12M",
     authDomain: "unote-cc660.firebaseapp.com",
@@ -22,6 +30,42 @@ function send_message(){
       })
     }
 
+
+function outdated_build(){
+  var error_screen = document.getElementById("outdated-ver")
+  userspace.style.display = "none"
+  error_screen.style.display = "block"
+}
+
+function outdated_security_patch(){
+  document.getElementById("error-message").innerHTML = "İhlal edilmiş güvenlik protokolü algılandı, çerezleri temizlemeyi deneyin"
+  outdated_build()
+}
+
+function provisionmode(){
+  document.getElementById("error-message").innerHTML = "Bakımdayız! Lütfen daha sonra tekrar gelin."
+  outdated_build()
+}
+
+
+function version_control(){
+  const userspace = document.getElementById("userspace")
+  db.ref("Ver/").on("value", function(version_object) {
+    version = version_object.val()
+    if(version["suspend"] == 1){
+      provisionmode()
+      return
+    }
+    if(version["major"] > major){
+      outdated_build()
+      return
+    }
+    if(version["security"] > security){
+      outdated_security_patch()
+      return
+    }
+  })
+}
 
 function get_name(){
     return localStorage.getItem("name")
@@ -76,13 +120,18 @@ function loads_chat(){
               }
             })
           })
-  
+          
+          var prev_name
           ordered.forEach(function(data) {
             var parent = document.getElementById("messagerecv")
             var name = data.name
             var message = data.message
             var message_element = document.createElement("p")
             var username_element = document.createElement("p")
+            if(c_init==false){
+              prev_name=name
+              c_init = null
+            }
             username_element.style.color = "red"
             username_element.style.fontSize = "20px"
             username_element.style.marginLeft = "25px"
@@ -93,18 +142,27 @@ function loads_chat(){
             message_element.style.marginBottom = "25px"
             message_element.style.marginTop = "-15px"
             if(name=="ingiltere" || name=="admin"){
-              username_element.style.color = "yellow";
-              username_element.style.backgroundColor = "black";
-              username_element.style.borderRadius = "15px 15px 0 0"
-              message_element.style.color = "black"
-              message_element.style.backgroundColor = "yellow"
-              message_element.style.borderRadius = "0 0 15px 15px"
-              message_element.style.marginLeft="25px"
-              message_element.style.marginTop="-23px"
-              message_element.style.paddingLeft = "10px"
+              var admin_tag = document.createElement("img")
+              admin_tag.src = "admin_tag.png"
+              admin_tag.style.width = "30px"
+              admin_tag.style.height = "30px"
+              admin_tag.style.marginBottom = "-7px"
+              username_element.style.color = "green"
+              username_element.append(admin_tag)
             }
-            parent.append(username_element)
+            if(prev_name == name){
+              if(c_init==null){
+                parent.append(username_element)
+                c_init=true
+              }
+            }
+            else{
+              parent.append(username_element)
+            }
+          prev_name = name
+            console.log(prev_name)
             parent.append(message_element)
+            
           })
           var msgDir = document.getElementById("msgshowroom")
           msgDir.scrollTop = msgDir.scrollHeight;
@@ -112,6 +170,7 @@ function loads_chat(){
 }
 
 window.onload = function() {
+  version_control()
     if(get_name() != null && parseInt(get_name().length)>0){
       autulogin=true
       getToChat()
@@ -136,9 +195,11 @@ window.onload = function() {
     send_message.onkeyup = function(){
         if(send_message.value.length > 0){
             send_message_button.style.display = "inline-block"
+            send_message.style.borderRadius="15px 0 0 15px"
         }
         else{
             send_message_button.style.display = "none"
+            send_message.style.borderRadius="15px"
         }
     }
     
